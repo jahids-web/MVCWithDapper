2f88fb680abd0c88b6809dc1537b79234d1eacdf
jahids-web <jahiduddin922@gmail.com>
2024-01-10 01:05:48 +0600

    Create New Controller AddProductController

---
+++ Dapper-Web/Dapper-Web/Controllers/AddProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dapper_Web.Controllers
+{
+    public class AddProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
