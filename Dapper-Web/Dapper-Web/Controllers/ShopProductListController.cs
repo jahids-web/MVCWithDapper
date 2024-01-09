246ffa20eb217d868feebd116d39dbcd902a6936
jahids-web <jahiduddin922@gmail.com>
2024-01-10 01:06:36 +0600

    Create New Controller ShopProductListController

---
+++ Dapper-Web/Dapper-Web/Controllers/ShopProductListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dapper_Web.Controllers
+{
+    public class ShopProductListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
