4f94efbb7b554acfb701ce765c8b0b241ef64e36
jahids-web <jahiduddin922@gmail.com>
2024-01-10 01:06:12 +0600

    Create New Controller CategorieController

---
+++ Dapper-Web/Dapper-Web/Controllers/CategorieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dapper_Web.Controllers
+{
+    public class CategorieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
