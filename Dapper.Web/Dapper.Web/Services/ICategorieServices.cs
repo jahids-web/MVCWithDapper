5f124b938150a945b8f880e21d00c48bb7d78d06
jahids-web <jahiduddin922@gmail.com>
2024-01-11 00:42:01 +0600

    Create Services CategorieServices

---
+++ Dapper.Web/Dapper.Web/Services/ICategorieServices.cs
@@ -0,0 +1,13 @@
+﻿using Dapper.Web.Entities;
+
+namespace Dapper.Web.Services
+{
+    public interface ICategorieServices
+    {
+        public Categorie Insert(Categorie categorie);
+        public IEnumerable<Categorie> GetAll(Categorie categorie);
+        public IEnumerable<Categorie> GetById(long Id);
+        public long Delete(long Id);
+        public void Update(Categorie categorie);
+    }
+}
