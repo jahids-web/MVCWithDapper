ac903b7d9444d2631ddb7f16f5f34432c7f717aa
jahids-web <jahiduddin922@gmail.com>
2024-01-11 00:43:25 +0600

    Create Repository Interface class

---
+++ Dapper.Web/Dapper.Web/Base/ICategorieRepository.cs
@@ -0,0 +1,14 @@
+﻿using Dapper.Web.Entities;
+
+namespace Dapper.Web.Base
+{
+    public interface ICategorieRepository
+    {
+        public Categorie Create(Categorie categorie);
+        public IEnumerable<Categorie> GetAll(Categorie categorie);
+        public IEnumerable<Categorie> GetById(long Id);
+        public long Delete(long Id);
+        public void Update(Categorie categorie);
+
+    }
+}
