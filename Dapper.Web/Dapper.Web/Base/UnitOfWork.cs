0cc979ec1419ee29ab2e8b43633bf1c2f998ea10
jahids-web <jahiduddin922@gmail.com>
2024-01-11 00:44:12 +0600

    Create UnitOfWork class

---
+++ Dapper.Web/Dapper.Web/Base/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Dapper.Web.DbContext;
+
+namespace Dapper.Web.Base
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ICategorieRepository _categorieRepository;
+        private readonly IDapperContext _dapperContext;
+
+        public UnitOfWork(ICategorieRepository categorieRepository, IDapperContext dapperContext)
+        {
+            _categorieRepository = categorieRepository;
+            _dapperContext = dapperContext;
+        }
+
+
+        public ICategorieRepository CategorieRepository => _categorieRepository ?? new CategorieRepository(_dapperContext);
+    }
+}
