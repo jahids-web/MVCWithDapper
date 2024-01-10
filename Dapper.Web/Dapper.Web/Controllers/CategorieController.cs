66e984f15bd24aaa5878cde4a2da53fbd0dc0565
jahids-web <jahiduddin922@gmail.com>
2024-01-11 00:41:17 +0600

    Add CRUD Mathod

--- Dapper.Web/Dapper.Web/Controllers/CategorieController.cs
+++ Dapper.Web/Dapper.Web/Controllers/CategorieController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper.Web.Entities;
+using Dapper.Web.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Dapper.Web.Controllers
 {
     public class CategorieController : Controller
     {
+        private readonly ICategorieServices _categorieServices;
+
+        public CategorieController(ICategorieServices categorieServices)
+        {
+            _categorieServices = categorieServices;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public Categorie Insert(Categorie categorie)
+        {
+            var result = _categorieServices.Insert(categorie);
+            return(result);
         }
     }
 }
